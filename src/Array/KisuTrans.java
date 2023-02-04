adb6fb9a4dd43e3b7e439384e48038be7a93c3f3
ororso11 <ororso135055@gmail.com>
2023-02-04 21:23:36 +0900

    1. 10진수 -> n진수 변환

---
+++ src/Array/KisuTrans.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class KisuTrans {
+    public static void main(String[] args) {
+        char[] res = new char[32];
+
+        int dno = test1(59,2,res);
+        for(int i = dno - 1; i >= 0; i--) {
+            System.out.println(res[i]);
+        }
+    }
+
+    public static int test1(int n , int r , char[] res) {
+        int cnt = 0;
+        // 알파벳 수
+        String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        do {
+            res[cnt++] = dchar.charAt( n % r);
+            n /= r;
+        } while ( n!=0);
+        return cnt;
+    }
+
+}
